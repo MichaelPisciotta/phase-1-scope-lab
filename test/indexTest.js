d68ec65fb49137a1da432f532605ada807f015f1
MichaelPisciotta <mfp56@miami.edu>
2021-07-22 16:23:30 -0400

    done

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,6 +5,9 @@ describe('Fix the Scope', function() {
     });
   });
 
+
+
+
   describe('upperCaseCustomerName()', function() {
     it('modifies the customerName variable', function() {
       expect(window.customerName).to.equal('bob');
@@ -14,6 +17,7 @@ describe('Fix the Scope', function() {
       expect(window.customerName).to.equal('BOB');
     });
   });
+
   describe('setBestCustomer()', function() {
     it('setBestCustomer', function() {
       expect(window.bestCustomer).to.equal(undefined);
@@ -23,6 +27,7 @@ describe('Fix the Scope', function() {
       expect(window.bestCustomer).to.equal('not bob');
     });
   });
+
 
   describe('overwriteBestCustomer()', function() {
     it('overwrites the best customer', function() {
